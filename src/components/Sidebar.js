07cb30a2aa9cd948ce5d50208ab7b239e08e7933
bbatus <batuhan2501@hotmail.com>
2024-08-03 16:07:56 +0300

    Improved sidebars behaviour

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,14 @@
-// src/components/Sidebar.js
 import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import '../assets/styles/Sidebar.css';
+import homeIcon from '../assets/images/home.svg'; // Anasayfa için simge
+import notificationIcon from '../assets/images/notification.svg'; // Bildirimler için simge
+import videoSolutionIcon from '../assets/images/videosolution.svg'; // Video Çözüm için simge
+import attendanceIcon from '../assets/images/attendance.svg'; // Yoklama için simge
+import homeworkIcon from '../assets/images/homework.svg'; // Ödev Takip için simge
+import guidanceIcon from '../assets/images/guidance.svg'; // Rehberlik için simge
+import registrationIcon from '../assets/images/registration.svg'; // Kayıt için simge
+import exitIcon from '../assets/images/exit.svg'; // Çıkış için simge
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -25,7 +32,8 @@ const Sidebar = () => {
             to="/dashboard/adminmainpage"
             className={({ isActive }) => (isActive ? 'active' : '')}
           >
-            <i className="fas fa-home"></i> Anasayfa
+            <img src={homeIcon} alt="Home Icon" className="sidebar-icon" />
+            <span>Anasayfa</span>
           </NavLink>
         </li>
         <li>
@@ -33,7 +41,8 @@ const Sidebar = () => {
             to="/dashboard/notifications"
             className={({ isActive }) => (isActive ? 'active' : '')}
           >
-            <i className="fas fa-bell"></i> Bildirimler
+            <img src={notificationIcon} alt="Notification Icon" className="sidebar-icon" />
+            <span>Bildirimler</span>
           </NavLink>
         </li>
         <li>
@@ -41,7 +50,8 @@ const Sidebar = () => {
             to="/dashboard/video-solution"
             className={({ isActive }) => (isActive ? 'active' : '')}
           >
-            <i className="fas fa-video"></i> Video Çözüm Modülü
+            <img src={videoSolutionIcon} alt="Video Solution Icon" className="sidebar-icon" />
+            <span>Video Çözüm Modülü</span>
           </NavLink>
         </li>
         <li>
@@ -49,7 +59,8 @@ const Sidebar = () => {
             to="/dashboard/attendance"
             className={({ isActive }) => (isActive ? 'active' : '')}
           >
-            <i className="fas fa-calendar-check"></i> Yoklama Modülü
+            <img src={attendanceIcon} alt="Attendance Icon" className="sidebar-icon" />
+            <span>Yoklama Modülü</span>
           </NavLink>
         </li>
         <li>
@@ -57,7 +68,8 @@ const Sidebar = () => {
             to="/dashboard/homework-tracking"
             className={({ isActive }) => (isActive ? 'active' : '')}
           >
-            <i className="fas fa-tasks"></i> Ödev Takip Modülü
+            <img src={homeworkIcon} alt="Homework Icon" className="sidebar-icon" />
+            <span>Ödev Takip Modülü</span>
           </NavLink>
         </li>
         <li>
@@ -65,7 +77,8 @@ const Sidebar = () => {
             to="/dashboard/guidance"
             className={({ isActive }) => (isActive ? 'active' : '')}
           >
-            <i className="fas fa-user-friends"></i> Rehberlik Modülü
+            <img src={guidanceIcon} alt="Guidance Icon" className="sidebar-icon" />
+            <span>Rehberlik Modülü</span>
           </NavLink>
         </li>
         <li>
@@ -73,12 +86,14 @@ const Sidebar = () => {
             to="/dashboard/registration"
             className={({ isActive }) => (isActive ? 'active' : '')}
           >
-            <i className="fas fa-user-plus"></i> Kayıt Modülü
+            <img src={registrationIcon} alt="Registration Icon" className="sidebar-icon" />
+            <span>Kayıt Modülü</span>
           </NavLink>
         </li>
         <li>
           <button className="logout-button" onClick={handleLogout}>
-            <i className="fas fa-sign-out-alt"></i> Çıkış
+            <img src={exitIcon} alt="Exit Icon" className="sidebar-icon" />
+            <span>Çıkış</span>
           </button>
         </li>
       </ul>
